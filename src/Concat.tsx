68a8203512dc58f8ffd63ee0a667489eaa8121e7
Balkan Güler <cezeri@gmail.com>
2018-11-26 10:44:18 +0300

    proxy simule edildi

--- src/Concat.tsx
+++ src/Concat.tsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 import AlphaEntry from './AlphaEntry';
 import Label from './Label';
@@ -11,65 +12,75 @@ interface State {
 
 class Concat extends React.Component<{}, State> {
 
-    buttonClicked = () => {
+    async buttonClicked() {
 
-      fetch('http://localhost:8080/concats', {
-        method: 'POST',
-        body: 'firstname=' + this.state.input1 + '&lastname=' + this.state.input2,
-        headers: {
-          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-          },
-      }).then(r => {
+        try {
+
+            let data1 = await this.proxyCall(this.state.input1, this.state.input2);
+            global.console.log(data1);
+
+            this.handleResponse(data1.toString());
+        } catch (e) {
+            // handle error
+        }
+    }
+
+    async proxyCall(var1: string, var2: string ): Promise<string> {
+        let r =  await fetch('http://localhost:8080/concats', {
+            method: 'POST',
+            body: 'firstname=' + var1  + '&lastname=' + var2,
+            headers: {
+                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+            },
+        });
+
         return r.text();
-      }).then(data => {
-        global.console.log(data);
-        this.handleResponse(data.toString());
-      });
     }
 
     handleResponse(value: string) {
 
-      this.setState({output: value});
+        this.setState({output: value});
     }
     handleChange1(e: React.ChangeEvent<HTMLInputElement>): void {
-      this.setState({input1: e.target.value});
+        this.setState({input1: e.target.value});
     }
 
     handleChange2(e: React.ChangeEvent<HTMLInputElement>): void {
-      this.setState({input2: e.target.value});
+        this.setState({input2: e.target.value});
     }
 
     handleChange3(e: React.ChangeEvent<HTMLInputElement>): void {
-      this.setState({output: e.target.value});
+        this.setState({output: e.target.value});
     }
 
     constructor() {
-      super({});
-      this.handleChange1 = this.handleChange1.bind(this);
-      this.handleChange2 = this.handleChange2.bind(this);
-      this.handleChange3 = this.handleChange3.bind(this);
-      this.handleResponse = this.handleResponse.bind(this);
-      
-      this.state = { count: 0, input1: '', input2: '', output: '' };
+        super({});
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleChange3 = this.handleChange3.bind(this);
+        this.handleResponse = this.handleResponse.bind(this);
+        this.buttonClicked = this.buttonClicked.bind(this);
+
+        this.state = { count: 0, input1: '', input2: '', output: '' };
     }
 
     render() {
-      return (
-        <div>
-          <div>
-              <AlphaEntry value={this.state.input1} onChange={this.handleChange1}/>
-              <AlphaEntry value={this.state.input2} onChange={this.handleChange2}/>
-          </div>
-          <div>
-            <button onClick={this.buttonClicked}>
-                Increment {name}
-            </button>
-            <Label value={this.state.output}/>
-         </div>
-        </div>
-      );
+        return (
+            <div>
+                <div>
+                    <AlphaEntry value={this.state.input1} onChange={this.handleChange1}/>
+                    <AlphaEntry value={this.state.input2} onChange={this.handleChange2}/>
+                </div>
+                <div>
+                    <button onClick={this.buttonClicked}>
+                        Concat1 {name}
+                </button>
+                <Label value={this.state.output}/>
+                </div>
+            </div>
+        );
     }
-  }
+}
 
 export default Concat;
